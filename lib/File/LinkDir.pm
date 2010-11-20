5f3109e472963a2725485eb429614db3cd796b98
Matthew Musgrove <mr.muskrat@gmail.com>
2010-11-20 15:41:15 -0600

    Release 1.00

--- lib/File/LinkDir.pm
+++ lib/File/LinkDir.pm
@@ -9,7 +9,7 @@ use File::Find;
 use File::Path qw<remove_tree make_path>;
 use File::Spec::Functions qw<catpath splitpath>;
 
-our $VERSION = '1.00_07';
+our $VERSION = '1.00';
 $VERSION = eval $VERSION;
 
 sub new
